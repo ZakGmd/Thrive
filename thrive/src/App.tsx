464d434a4361870a5af500ddde019dec08462b00
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-17 23:50:28 +0100

    initialize a new project , we will work with react and r3f and tailwind , we'll see

---
+++ thrive/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='flex flex-col w-full h-[100vh] bg-neutral-950'>
+      <div className=" flex items-center justify-between px-3 pt-3">
+        <div className=" font-bold text-2xl px-6 text-slate-300 ">Thrive</div>
+        <button className=" font-bold text-xl px-6 py-1 text-slate-300 border-2 border-cyan-800 rounded-full ">Get all things free</button>
+       </div>
+    </div>
+         
+    </>
+  )
+}
+
+export default App
---
+++ thrive/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+  			'space-mono': [
+  				'Space Mono',
+  				'serif'
+  			],
+  			inter: [
+  				'Inter',
+  				'serif'
+  			]
+  		},
+    },
+  },
+  plugins: [],
+}
+
