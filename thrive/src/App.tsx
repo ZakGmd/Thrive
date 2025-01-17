8f73609bbe2da9e90d527807cbc908fc0df9bfab
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-18 00:29:23 +0100

    adding a simple nav !

--- thrive/src/App.tsx
+++ thrive/src/App.tsx
@@ -5,10 +5,19 @@ function App() {
 
   return (
     <>
-    <div className='flex flex-col w-full h-[100vh] bg-neutral-950'>
+    <div className='flex flex-col w-full h-[100vh] bg-neutral-950 font-inter'>
       <div className=" flex items-center justify-between px-3 pt-3">
-        <div className=" font-bold text-2xl px-6 text-slate-300 ">Thrive</div>
-        <button className=" font-bold text-xl px-6 py-1 text-slate-300 border-2 border-cyan-800 rounded-full ">Get all things free</button>
+        <div className=" font-bold text-2xl px-6 text-slate-50 ">Thrive</div>
+        <div className='flex items-center text-white gap-10'>
+           <div>Features</div>
+           <div>How it works</div>
+           <div>Pricing</div>
+           <div>About</div>
+        </div>
+        <div className='flex items-center gap-2 '>
+            <div>Blog</div>
+            <div className='flex bg-gradient-to-b from-white/20 to-white/30 backdrop-blur-sm  px-3 py-1 rounded-lg text-white cursor-pointer  transition-colors duration-300 '>Download Soon</div>
+        </div>
        </div>
     </div>
          
