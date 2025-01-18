1ccffc67316a64f1ffdd6f83fa4669f2d23aabe1
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-18 03:52:48 +0100

    adjusting the lines animation , it looks good now !

--- thrive/src/App.tsx
+++ thrive/src/App.tsx
@@ -1,13 +1,51 @@
 import './App.css'
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
+
 
 function App() {
+  const container = useRef(null) ;
 
-
+   useGSAP(()=>{
+    const tl = gsap.timeline() ;
+    tl.fromTo(".lines",{
+      opacity: 0 ,
+      duration: 1, 
+      y:-180 ,
+    },{
+      opacity: 1 ,
+      duration: 2, 
+      y:0 ,
+      ease: "power2.out"
+    }).fromTo(".leftLine",{
+      opacity: 0 ,  
+      duration: 1 ,
+      x:-120 ,
+    },{
+      x:0 ,
+      opacity:1 ,
+      duration: 2 ,
+      ease: "power2.out"
+    },"<").fromTo(".rightLine",{
+      opacity: 0 ,  
+      duration: 1 ,
+      x:120 ,
+    },{
+      x:0 ,
+      opacity:1 ,
+      duration: 2 ,
+      ease: "power2.out"
+  },"<")
+   },{scope: container})
   return (
     <>
-    <div className='flex flex-col w-full h-[100vh] bg-neutral-950 font-inter'>
+    <div className='flex flex-col w-full h-[100vh] bg-neutral-950 font-inter' ref={container} >
       <div className=" flex items-center justify-between px-3 pt-4">
-        <div className=" font-bold text-2xl px-6 text-neutral-50 bg-clip-text  text-transparent bg-gradient-to-r from-[#F27500] from-[10%] to-neutral-100 to-[45%] contrast-125  ">Thrive</div>
+        <div className=" font-bold text-2xl px-6 text-neutral-50 bg-clip-text  text-transparent bg-gradient-to-r from-[#F27500] from-[10%] to-neutral-100 to-[45%] contrast-125 w-[200px]  ">Thrive</div>
         <div className='flex items-center gap-10 '>
            <div className='text-[#F27500] cursor-pointer contrast-125'>Features</div>
            <div className='text-white/25 cursor-pointer hover:text-[#F27500]/55 transition-all duration-150 '>How it works</div>
@@ -20,19 +58,19 @@ function App() {
         </div>
        </div>
        <div className='relative mt-[200px] max-w-[1400px] w-full mx-auto flex flex-col items-center justify-between h-full    '>
-           <div className=" absolute w-[1px] left-[340px] top-[-100px] h-[182px] bg-gradient-to-b from-transparent  to-[120%]  to-[#F27500]/30 rounded-2xl"></div>
-           <div className=" absolute w-[1px] right-[360px] top-[-100px] h-[182px] bg-gradient-to-b from-transparent to-[120%] to-[#F27500]/30 rounded-2xl"></div>
-           <div className=" absolute h-[1px] left-[100px] top-[200px] w-[200px] bg-gradient-to-r from-transparent  to-[120%]  to-[#F27500]/30">
+           <div className="lines absolute w-[1px] left-[340px] top-[-110px] h-[182px] bg-gradient-to-b from-transparent  to-[120%]  to-[#F27500]/30 rounded-2xl"></div>
+           <div className="lines absolute w-[1px] right-[360px] top-[-110px] h-[182px] bg-gradient-to-b from-transparent to-[120%] to-[#F27500]/30 rounded-2xl"></div>
+           <div className="leftLine absolute h-[1px] left-[100px] top-[200px] w-[200px] bg-gradient-to-r from-transparent  to-[120%]  to-[#F27500]/30">
                   <div className=" absolute right-[-0.1px] bottom-[-1.80px] rounded-full w-[4.3px] h-[4.7px] bg-[#F27500]/50"></div>
           </div>
-          <div className=" absolute h-[1px] right-[100px] top-[200px] w-[200px] bg-gradient-to-l from-transparent  to-[120%]  to-[#F27500]/30">
+          <div className="rightLine absolute h-[1px] right-[100px] top-[200px] w-[200px] bg-gradient-to-l from-transparent  to-[120%]  to-[#F27500]/30">
                  <div className=" absolute left-[-0.9px] bottom-[-1.85px] rounded-full w-[4.3px] h-[4.7px] bg-[#F27500]/50"></div>
           </div>
 
           <div className='flex flex-col items-center gap-10'>
             <div className="flex flex-col gap-3 py-2 items-center bg-clip-text  text-transparent bg-gradient-to-r from-neutral-100  from-[10%] to-[#F27500] to-[95%] contrast-125 ">
                     <span className=" text-7xl  font-semibold  "  >Eternal Wisdom,</span>
-                    <span className="bg-clip-text  text-transparent bg-gradient-to-r from-neutral-100  from-[10%] to-[#F27500] to-[95%] contrast-125 font-normal text-lg">for Today's Mind</span>
+                    <span className="text-neutral-50  contrast-125 font-normal text-lg">for Today's Mind</span>
             </div>
             <div className='flex flex-col items-center gap-10'>
                <div className="text-neutral-50 font-normal contrast-50 brightness-50  text-lg max-w-[480px] text-center">Experience meditation reimagined through AI guidance, making mindfulness a natural part of your day</div>
