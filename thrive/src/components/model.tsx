b181eb70af53054355878278b8004f174fe3093d
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-21 22:51:20 +0100

    founding the screen node element , so we'll make some art inside it !

--- thrive/src/components/model.tsx
+++ thrive/src/components/model.tsx
@@ -1,13 +1,25 @@
 import { useGLTF } from "@react-three/drei"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import * as THREE from 'three'
 import { useFrame } from "@react-three/fiber"
 
 export default function Model() {
     const { nodes, scene } = useGLTF('/iphone.glb')
     const modelRef = useRef<THREE.Group>(null)
-
-
+   
+    console.log({nodes})
+    useEffect(()=>{
+        if (nodes.defaultMaterial_12) {
+            (nodes.defaultMaterial_12 as THREE.Mesh).material = new THREE.MeshPhysicalMaterial({
+                color: new THREE.Color('#f59e0b'),
+                transparent: true,
+                opacity: 1,
+                metalness: 0.9,
+                roughness: 0.1,
+                
+            })
+        }
+    })
     return (
         <group
             ref={modelRef}
