7dbe49dc89d6a5f1014a9e906bd5b00a6a9c3d2d
Ghoumidate zakaria <ghoumidatezakaria@gmail.com>
2025-01-22 04:30:00 +0100

    adjusting the design of the model , it looks clean now !

--- thrive/src/components/model.tsx
+++ thrive/src/components/model.tsx
@@ -21,19 +21,23 @@ export default function Model() {
       <Html
         transform
         distanceFactor={7}
-        position={[-0.3, -0.13, 0.25]}
+        position={[-0.3, 0.01, 0.32]}
         rotation={screenMesh.rotation}
         
       >
-        <div ref={htmlRef} style={{ width: '409px', height: '902px' }}>
-           <div className='h-full w-full flex flex-col items-start  rounded-[48px] overflow-hidden font-inter'>
-             <div className='flex items-center justify-between text-white pt-3 w-full'>
-                <div className=' max-w-[20px] pl-5 w-full'>22:10</div>
+        <div ref={htmlRef} style={{ width: '411.8px', height: '892px' }}>
+           <div className='h-full w-full flex flex-col items-start bg-gradient-to-b from-transparent from-[30%] to-[#F27500]/10  rounded-[48px] overflow-hidden font-inter'>
+             <div className='flex items-center justify-between  pt-3 w-full'>
+                <div className=' max-w-[20px] pl-5 w-full text-[14px] text-white contrast-150'>22:10</div>
                 <div className='w-[120px] ml-[11px] mt-1 h-[37.5px] rounded-full bg-black/5 '></div>
-                <div>ssss</div>
+                <div className='flex items-center gap-2 pr-5'>
+                    <img src="signal.svg" alt="" />
+                    <img src="wi-fi.svg" alt="" />
+                    <img src="battery.svg" alt="" />
+                </div>
              </div>
              <div className='pt-2'>
-                <img src='wallpaper1.jpg' />
+               
              </div>
            </div>
         </div>
